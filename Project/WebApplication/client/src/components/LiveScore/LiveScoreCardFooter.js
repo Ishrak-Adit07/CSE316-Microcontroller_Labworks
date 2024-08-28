9673bcff3aac9ea3c3be571332f27f7f625ee287
Ishrak Adit <ishrak.adit07@gmail.com>
2024-08-28 12:23:02 +0600

    Project web app client cleanup commit

--- Project/WebApplication/client/src/components/LiveScore/LiveScoreCardFooter.js
+++
@@ -1,9 +0,0 @@
-const RegisterCardFooter = () =>{
-    return (
-        <div>
-            <p style={{color:'white'}}>Copyright @ Ishrak-Adit@Gryffindor</p>
-        </div>
-    );
-  }
-
-  export default RegisterCardFooter;
--- Project/WebApplication/client/src/components/Login/LoginCardFooter.js
+++
@@ -1,9 +0,0 @@
-const LoginCardFooter = () =>{
-    return (
-        <div>
-            <p style={{color:'white'}}>Copyright @ Ishrak-Adit@Gryffindor</p>
-        </div>
-    );
-  }
-
-  export default LoginCardFooter;
